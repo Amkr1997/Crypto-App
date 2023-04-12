7c37d467d8a0bd1c3492809d48bfc25e0ed69f27
Aman Khare <113411805+Amkr1997@users.noreply.github.com>
2023-04-12 11:02:48 +0530

    Add files via upload

--- src/ApiConfig/api.jsx
+++ src/ApiConfig/api.jsx
@@ -1,1 +1,11 @@
+export const coinsData = (currency) => {
+  return `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en`;
+};
 
+export const singleCoinData = (id) => {
+  return `https://api.coingecko.com/api/v3/coins/${id}`;
+};
+
+export const historicalChartData = (id, days = 365) => {
+  return `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=inr&days=${days}`;
+};
