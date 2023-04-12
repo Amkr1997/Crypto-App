ea1e834da9c0ad4113b152001a94491d43983cf5
Aman Khare <113411805+Amkr1997@users.noreply.github.com>
2023-04-12 11:16:06 +0530

    Add files via upload

--- src/reducers/CryptoReducer.jsx
+++ src/reducers/CryptoReducer.jsx
@@ -1,1 +1,41 @@
+const CryptoReducer = (cryptoState, action) => {
+  if (action.type === "SET_LOADING") {
+    return {
+      ...cryptoState,
+      isLoading: true,
+    };
+  }
 
+  if (action.type === "SET_DATA") {
+    return {
+      isLoading: false,
+      ...cryptoState,
+      cryptoData: action.payload,
+      isError: false,
+    };
+  }
+
+  if (action.type === "SET_ERROR") {
+    return {
+      ...cryptoState,
+      isError: true,
+    };
+  }
+  /*
+  if (action.type === "SET_SINGLE_CRYPTO") {
+    return {
+      ...cryptoState,
+      singleCryptoData: action.payload,
+    };
+  }
+
+  if (action.type === "SET_HISTORICAL_DATA") {
+    return {
+      ...cryptoState,
+      cryptoMarketData: action.payload,
+    };
+  }
+  */
+};
+
+export default CryptoReducer;
