f88a38462436accd0feeb49a5b0fdd15cd391d25
Aman Khare <113411805+Amkr1997@users.noreply.github.com>
2023-04-12 11:04:04 +0530

    Add files via upload

--- src/ChartConfig/ChartDays.jsx
+++ src/ChartConfig/ChartDays.jsx
@@ -1,1 +1,34 @@
+import React from "react";
 
+const ChartDays = [
+  {
+    label: "24h",
+    value: 1,
+  },
+  {
+    label: "7d",
+    value: 7,
+  },
+  {
+    label: "30d",
+    value: 30,
+  },
+  {
+    label: "90d",
+    value: 90,
+  },
+  {
+    label: "180d",
+    value: 180,
+  },
+  {
+    label: "365d",
+    value: 365,
+  },
+  {
+    label: "max",
+    value: "max",
+  },
+];
+
+export default ChartDays;
